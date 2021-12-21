ae6c245c98a2af0a6e5903092abf38d7d4f9aae6
unknown <nolan.beihl@gmail.com>
2021-12-21 12:47:33 -0700

    Fixed randomization of nearbyRestaurants, working on making nearbyEntertainment more specific simliar to restaurants

--- spontanious-app/src/App.jsx
+++ spontanious-app/src/App.jsx
@@ -9,22 +9,23 @@ class App extends Component{
         this.state= {
             apiKey : 'AIzaSyBwQzBmt6jC2y_0yA8R3Cr9EduNwQL0hrQ',
             address : '',
-            lat: 23.000,
-            lng: 115.000,
+            lat: 38.9661395,
+            lng: -104.6005025,
             anotherKey: 'AIzaSyB1j0XHBYGZI5Pi0ryYwSb29NQNWp3uqMo',
-            readableAddress : ''
-
+            readableAddress : '',
+            restaruantPick : [],
+            entertainmentPick : [],
         }
     }
 
     componentDidMount(){
-        this.testlocation()
-        this.convertlocation()
-        this.nearbyRestaurant()
+        // this.testlocation()
+        // this.convertlocation()
+        // this.nearbyRestaurant()
     }
 
 
-    testlocation = async() =>{ 
+    explorerLocation = async() =>{ 
         let response = await axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${this.state.apiKey}`)
         this.setState({
             lat: (response.data.location.lat),
@@ -32,43 +33,57 @@ class App extends Component{
         })
     }
     
-    convertlocation = async() =>{
+    convertLocation = async() =>{
         let response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${this.state.lat},${this.state.lng}&key=${this.state.anotherKey}`)
         this.setState({
             address : [response.data.data],
             readableAddress : (response.data.results[0].formatted_address),
         })
-
         alert(response.data.results[0].formatted_address)
-     
     }
     nearbyRestaurant = async() =>{
         let response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${this.state.lat}%2C${this.state.lng}&radius=5000&type=restaurant&key=AIzaSyB1j0XHBYGZI5Pi0ryYwSb29NQNWp3uqMo`)
         this.setState({
             restaurants : [response.data]
         })
-        debugger;
-        alert(response.data)
+        var choice = [response.data.results];
+        var randomchoice = choice[Math.floor(Math.random()*choice.length)];
+        var anotherchoice = randomchoice[Math.floor(Math.random()*randomchoice.length)];
+        alert(`${anotherchoice.name}, ${anotherchoice.rating}, ${anotherchoice.price_level}`)
+        this.setState({
+            restaruantPick : [anotherchoice]
+        })
     }
-    
-    
+    nearbyEntertainment = async() =>{
+        let response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${this.state.lat}%2C${this.state.lng}&radius=5000&type=entertainment&key=AIzaSyB1j0XHBYGZI5Pi0ryYwSb29NQNWp3uqMo`)
+        this.setState({
+            entertainments : [response.data]
+        })
+        var choice = [response.data.results];
+        var randomchoice = choice[Math.floor(Math.random()*choice.length)];
+        var anotherchoice = randomchoice[Math.floor(Math.random()*randomchoice.length)];
+        alert(`${anotherchoice.name}, ${anotherchoice.rating}, ${anotherchoice.price_level}`)
+        this.setState({
+            entertainmentPick : [anotherchoice]
+        })
+    }
+  
 
     render() {
   
         return(
-            <div className = 'container'>
-                <h1>Spontaneous</h1>
-                <div><p>{this.state.lat}</p></div>
-                <div><p>{this.state.lng}</p></div>
-                <button onClick={this.convertlocation}>Convert Location</button>
-                <button onClick={this.nearbyRestaurant}>Find restaurants</button>
+            <div className='container'>
+                <h1>Spontaneous</h1>   
+                <button onClick={this.explorerLocation}>Get Location</button>
+                <button onClick={this.convertLocation}>Convert Location</button>
+                <button onClick={this.nearbyRestaurant}>Find Restaurants</button>
+                <button onClick={this.nearbyEntertainment}>Find Entertainment</button>
                 <div><p>{this.state.readableAddress}</p></div>
             </div>
-            );
-           
-    }
+                
+        )
 
-}
+}}
 
 export default App;
 
