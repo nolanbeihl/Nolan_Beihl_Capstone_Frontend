bed120150162927aacfa62c24869f3df2bd07966
unknown <nolan.beihl@gmail.com>
2021-12-21 09:14:17 -0700

    Refined the convertlocation function, tested with an alert popup to confirm functionality.  Moving on to the Google Places API

--- spontanious-app/src/App.jsx
+++ spontanious-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useDebugValue } from "react";
 import axios from 'axios';
 import {Component} from "react";
 
@@ -9,8 +9,8 @@ class App extends Component{
         this.state= {
             apiKey : 'AIzaSyBwQzBmt6jC2y_0yA8R3Cr9EduNwQL0hrQ',
             address : '',
-            lat: 0,
-            lng: 0,
+            lat: 23.000,
+            lng: 115.000,
             anotherKey: 'AIzaSyB1j0XHBYGZI5Pi0ryYwSb29NQNWp3uqMo',
 
         }
@@ -18,11 +18,11 @@ class App extends Component{
 
     componentDidMount(){
         this.testlocation()
-        // this.convertlocation()
+        this.convertlocation()
     }
 
 
-    testlocation = async() =>{
+    testlocation = async() =>{ 
         let response = await axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${this.state.apiKey}`)
         this.setState({
             lat: (response.data.location.lat),
@@ -33,31 +33,33 @@ class App extends Component{
         // this.setState({
         //     address : [readableAddress.data.location],
         // })
-      
-        // console.log(readableAddress.data.results)
+        // console.log[readableAddress.data]
     }
     
     convertlocation = async() =>{
         let response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${this.state.lat},${this.state.lng}&key=${this.state.anotherKey}`)
         this.setState({
-            address : (response.data)
+            address : [response.data.data],
         })
-        debugger;
-        console.log(response.data)
+        console.log('converted locaiton: ',response.data.results)
+        alert(response.data.results[0].formatted_address)
      
     }
+    
+    
 
     render() {
+  
         return(
             <div className = 'container'>
                 <h1>Spontaneous</h1>
                 <div><p>{this.state.lat}</p></div>
                 <div><p>{this.state.lng}</p></div>
-                <button onClick={this.convertlocation()}>Convert Location</button>
+                <button onClick={this.convertlocation}>Convert Location</button>
                 <div><p>{this.state.address}</p></div>
             </div>
             );
-            debugger;
+           
     }
 
 }
