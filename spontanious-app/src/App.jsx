61c1121e784b8db1b9d02972ed01de049a028675
Nolan Beihl <nolan.beihl@gmail.com>
2022-03-31 14:22:29 -0600

    Changed default entertainment type to tourist_attraction to increase chance of not returning lodging

--- spontanious-app/src/App.jsx
+++ spontanious-app/src/App.jsx
@@ -24,7 +24,7 @@ class App extends Component{
             lng: -77.0369,
             anotherKey: 'AIzaSyB1j0XHBYGZI5Pi0ryYwSb29NQNWp3uqMo',
             readableAddress : '',
-            entertainmentPick : '',
+            entertainmentPick : 'tourist_attraction',
             distance : 0,
             optionPicked : '0',
             rest_review : '0',
